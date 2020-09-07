0d251bd789f97a699848f7e1690e65a6b6dbf2e6
vidura umayanaga perera <laumayangaperera@gmail.com>
2020-09-07 14:54:20 +0530

    vup test_1 in git

--- Ambrosia_Project/models.py
+++ Ambrosia_Project/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 # Create your models here.
+
+
 class Broker(models.Model):
     name = models.CharField(max_length=50)
     address = models.TextField(null=True)
     phone = models.CharField(max_length=10, null=True)
 
+
 class Buyer(models.Model):
     vat_regno = models.CharField(max_length=30)
     name = models.CharField(max_length=50)
+
+
+class Funds(models.Model):
+    date = models.DateField(input_formats=['%d/%m/%Y'])
+    emp_
