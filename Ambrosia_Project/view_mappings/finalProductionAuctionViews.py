2fbb7bad5dce09e7c04e56a11d39d5bfbac1ddfb
Sandun01 <sandunlakshitha06@gmail.com>
2020-10-06 19:11:18 +0530

    moved common utils and renamed to auctionStockUtills - Sandun

--- Ambrosia_Project/view_mappings/finalProductionAuctionViews.py
+++ Ambrosia_Project/view_mappings/finalProductionAuctionViews.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from Ambrosia_Project.forms import *
-from Ambrosia_Project.commonUtills import *
+from Ambrosia_Project.common_utills.auctionStockUtills import *
 from django.views.generic import View
 
 #Auction stock--------------------------------------------------------------------------------------------
