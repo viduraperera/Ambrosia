82583d9d5a010d020be5fc8f8971418cd933f3a8
Sandun01 <sandunlakshitha06@gmail.com>
2020-10-10 11:41:23 +0530

    fixed merge issues in tea shopInventory

--- Ambrosia_Project/views.py
+++ Ambrosia_Project/views.py
@@ -147,7 +147,7 @@ def DeleteUser(request):
 
 @login_required(login_url='login')
 def inventoryhome (request):
-    return render(request, 'TeaShopInventory_Templates/inventoryhome.html')
+    return render(request, 'TeaShopInventory_Templates/../templates/inventoryhome.html')
 
 
 @login_required(login_url='login')
