62da94cd78231552c531a33fefe48fc18f520c21
Sandun01 <sandunlakshitha06@gmail.com>
2020-10-10 16:37:22 +0530

    before merge

--- Ambrosia_Project/urls.py
+++ Ambrosia_Project/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-from . views import *
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -15,39 +14,11 @@ urlpatterns = [
     path('Factory', views.factoryHomepage, name='factory_home'),
     path('Shop', views.teashopHomepage, name='teashop_home'),
 
-    path('Factory/EmployeeHome', views.EmployeeHome, name="EmployeeHome"),
-
 
     # transport management
+# ---------------------------------------------------------------------------------------------------------------
+    path('Factory/Transport', include("Ambrosia_Project.url_mappings.transportationUrls"))
 
-    # Driver
-    path('Factory/Transport', views.driver_records, name="Transport"),
-    path('Factory/Transport/UpdateDriver', views.DisplayDriverRecord, name="ShowDriver"),
-    path('Factory/Transport/DeleteDriverRecords', views.deleteDriverRecord, name="delete_Driver_record"),
-    path('Factory/Transport/Records', views.UpdateDriverRecord, name="update_Driver_record"),
-
-    # Vehicle
-    path('Factory/Transport/AddVehicle', views.Vehicle_Records, name="addVehicle"),
-    path('Factory/Transport/DeleteVehicleRecords', views.deleteVehicleRecord, name="delete_record"),
-
-    #Driving records
-    path('Factory/Transport/DrivingRecords', views.DrivingRecords, name="vehicle_records"),
-    path('Factory/Transport/ShowRecords', views.ShowDrivingRecords, name="RecordTable"),
-    path('Factory/Transport/DeleteRecords', views.deleteRecords, name="delete_driving_records"),
-
-
-    #vehicle repairs
-    path('Factory/Transport/VehicleRepairs', views.AddVehicleRepairs, name="VehicleRepairs"),
-    path('Factory/Transport/ShowVehicleRepairs', views.ShowVehicleRepairs, name="RepairTable"),
-    path('Factory/Transport/EditVehicleRepairs', views.DisplayUpdateRepairs, name="show_repair_records"),
-
-    path('Factory/Transport/UpdateVehicleRepairs', views.UpdateVehicleRepairs, name="update_repair_records"),
-    path('Factory/Transport/deleteVehicleRepairs', views.delete_RepairRecords, name="delete_repair_records"),
-
-    #reports
-    path('Factory/Transport/Analysis', views.Reports, name="Reports"),
-    path('Factory/Transport/Analysis/VehicleRecordsPDF', GenerateVehicle_RecordsPdf.as_view(), name="VehicleReports"),
-    path('Factory/Transport/Analysis/VehicleRepairsPDF', GenerateVehicle_RepairPdf.as_view(), name="VehicleReports"),
 
 ]
 
---
+++ Ambrosia_Project/view_mappings/transportationViews.py
@@ -0,0 +1,425 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from Ambrosia_Project.forms import *
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from Ambrosia_Project.utils import render_to_pdf
+from django.views import View
+
+# Create your views here.
+
+#transport management------------------------------------------------------------------------------------------------------------
+
+#add vehicle
+@login_required(login_url='login')
+def Vehicle_Records(request):
+
+    form = AddVehicle()
+
+    if request.method == 'POST':
+
+        form = AddVehicle(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('addVehicle')
+            except Exception as e:
+                print(e)
+                pass
+
+    #assign all vehicle objects
+    array = Vehicle.objects.all()
+
+    #assign form
+    var = {'vehicleForm': form}
+
+
+    alldetails = {'Vform':var , 'AllVehicle':array }
+
+    return render(request, 'Transport_templates/AddVehicle.html', alldetails)
+
+#delete vehicle
+@login_required(login_url='login')
+def deleteVehicleRecord(request):
+
+    id = request.POST.get('id')
+
+    if request.method == 'POST' and id != None:
+        vehicle = Vehicle.objects.get(id=id)
+        vehicle.delete()
+
+    return redirect('addVehicle ')
+
+
+
+#add driver
+
+@login_required(login_url='login')
+def driver_records(request):
+
+    form = AddDriver()
+
+    if request.method == 'POST':
+
+        form = AddDriver(request.POST)
+
+        if form.is_valid():
+            try:
+                # epfno = form.cleaned_data("epfNo")
+                # result = checkEpNo(epfno)
+
+                form.save()
+                messages.success(request, 'Successfully added driver details')
+                return redirect('Transport')
+
+            except Exception as e:
+                print(e)
+                messages.success(request, 'Exception:'+ e)
+
+        else:
+            print(form.errors)
+            messages.success(request, 'Invalid Details')
+
+
+    #assign all vehicle objects
+    array = Driver.objects.all()
+
+    #assign form
+    var = {'driverForm': form}
+
+
+    allDdetails = {'VDform':var , 'AllDriver':array }
+
+    return render(request, 'Transport_templates/AddDriver.html', allDdetails)
+
+#delete driver
+@login_required(login_url='login')
+def deleteDriverRecord(request):
+
+    id = request.POST.get('id')
+
+    if request.method == 'POST' and id != None:
+        driver = Driver.objects.get(id=id)
+        driver.delete()
+
+    return redirect('Transport')
+
+#display driver
+@login_required(login_url='login')
+def DisplayDriverRecord(request):
+
+    if request.method == 'POST':
+        id = request.POST.get('Did')
+
+        if id is not None:
+            try:
+                driver = Driver.objects.get(pk=id)
+                driverForm = AddDriver(instance=driver)
+                return render(request, 'Transport_templates/UpdateDriver.html', {'Dform': driverForm, 'DId':id})
+
+            except Exception as e:
+                print(e)
+
+        else:
+            pass
+
+    else:
+        pass
+
+    return redirect('Transport')
+
+#update driver records
+@login_required(login_url='login')
+def UpdateDriverRecord(request):
+
+    if request.method == 'POST':
+        dID = request.POST.get('DID')
+
+        if dID is not None:
+
+            try:
+                driver = Driver.objects.get(pk=dID)
+                form_update = AddDriver(request.POST, instance=driver)
+
+                if form_update.is_valid():
+                    form_update.save()
+                    return redirect('AddDriver')
+
+                else:
+                    #invalid
+                    pass
+
+            except Exception as e:
+                print(e)
+
+        else:
+            #id not found
+            pass
+
+    return redirect('Transport')
+
+
+
+#add vehicle driving records
+@login_required(login_url='login')
+def DrivingRecords(request):
+
+    form = VehicleRecordsForm()
+    diff = 0
+
+    if request.method == 'POST':
+
+        form = VehicleRecordsForm(request.POST)
+
+        if form.is_valid():
+            try:
+                start = form.cleaned_data['Start_Reading']
+                end = form.cleaned_data['End_Reading']
+                diff = int(end) - int(start)
+
+                if diff > 0:
+
+                    form_mread = form.save(commit=False)
+                    form_mread.Meter_Difference = diff
+
+                    form.save()
+                    messages.success(request, 'Successfully added driver details')
+                    return redirect('RecordTable')
+
+                else:
+                    messages.error(request, 'End reading Number Error')
+
+            except Exception as e:
+                print(e)
+                messages.error(request, 'Exception:' + e)
+
+        else:
+            print(form.errors)
+            messages.error(request, 'Invalid Details')
+
+    # assign form
+    var = {'RecordForm': form}
+
+    return render(request, 'Transport_templates/VehicleRecords.html', var)
+
+#show driving records
+@login_required(login_url='login')
+def ShowDrivingRecords(request):
+
+    records = Driving_Records.objects.all()
+
+    return render(request, 'Transport_templates/VehicleRecordsTable.html', {'records': records})
+
+
+
+#delete vehicle driving records
+@login_required(login_url='login')
+def deleteRecords(request):
+
+    id = request.POST.get('id')
+
+    if request.method == 'POST' and id != None:
+        record = Driving_Records.objects.get(id=id)
+        record.delete()
+
+    return redirect('RecordTable')
+
+
+#add vehicle repairs
+@login_required(login_url='login')
+def AddVehicleRepairs(request):
+
+    form = RepairForm()
+
+    if request.method == 'POST':
+
+        form = RepairForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('RepairTable')
+
+            except Exception as e:
+                print(e)
+
+                pass
+
+
+    #assign form
+    var = {'repairForm': form}
+
+    return render(request, 'Transport_templates/VehicleRepairs.html', var)
+
+
+@login_required(login_url='login')
+def ShowVehicleRepairs(request):
+
+    repairs = Services.objects.all()
+
+    return render(request, 'Transport_templates/RepairTable.html', {'repairs': repairs})
+
+#display repairs
+@login_required(login_url='login')
+def DisplayUpdateRepairs(request):
+
+    if request.method == 'POST':
+
+        id = request.POST.get('ReId')
+
+        if id is not None:
+            try:
+                repair = Services.objects.get(pk=id)
+                rForm = RepairForm(instance=repair)
+                return render(request, 'Transport_templates/EditRepairTable.html', {'REform': rForm, 'ReId':id})
+
+            except Exception as e:
+                print(e)
+
+        else:
+            pass
+
+    else:
+        pass
+
+    return redirect('RepairTable')
+
+
+
+#update vehicle repairs
+@login_required(login_url='login')
+def UpdateVehicleRepairs(request):
+
+
+    if request.method == 'POST':
+        id = request.POST.get('repairID')
+
+        if id is not None:
+
+            try:
+                repair = Services.objects.get(pk=id)
+                form_update = RepairForm(request.POST, instance=repair)
+
+                if form_update.is_valid():
+                    form_update.save()
+
+                else:
+                    #invalid
+                    pass
+
+            except Exception as e:
+                print(e)
+
+        else:
+            #id not found
+            pass
+
+    return redirect('RepairTable')
+
+
+#delete repair details
+@login_required(login_url='login')
+def delete_RepairRecords(request):
+
+    id = request.POST.get('id')
+
+    if request.method == 'POST' and id != None:
+        repair = Services.objects.get(id=id)
+        repair.delete()
+
+    return redirect('RepairTable')
+
+
+#reports
+
+@login_required(login_url='login')
+def Reports(request):
+
+    return render(request, 'Transport_templates/transportation_Reports.html')
+
+#generate vehicle reports
+class GenerateVehicle_RecordsPdf(View):
+    def get(self, request, *args, **kwargs):
+
+        vehicleNoInput = request.GET.get('vehicleno')
+        month = request.GET.get('month')
+        year = request.GET.get('year')
+        vehicleNo = 0
+
+        if len(month)== 2 and len(year)== 4:
+
+            vehicle = Vehicle.objects.filter(VehicleNo=vehicleNoInput).first()
+
+            if vehicle is not None:
+                vehicleNo = vehicle.id
+
+            records = Driving_Records.objects.filter(Date__month=month, Date__year=year , VehicleNo=vehicleNo)
+
+            if len(records) > 0:
+
+                total = 0
+
+                for rec in records:
+                    total = total + rec.Meter_Difference
+
+                data = {
+                    'Vrecords': records,
+                    'year':year,
+                    'month':month,
+                    'total': total,
+                    }
+
+                pdf = render_to_pdf('Transport_templates/Vehicle_Reports.html', data)
+                return HttpResponse(pdf, content_type='application/pdf')
+
+            else:
+                messages.error(request, 'No Data Found')
+                return redirect('Reports')
+
+        else:
+            messages.error(request, 'Invalid Inputs')
+            return redirect('Reports')
+
+
+#generate vehicle maintenance reports
+
+class GenerateVehicle_RepairPdf(View):
+    def get(self, request, *args, **kwargs):
+
+        vehicleNoInput = request.GET.get('vehicleno')
+        month = request.GET.get('month')
+        year = request.GET.get('year')
+        vehicleNo = 0
+
+        if len(month)== 2 and len(year)== 4:
+
+            vehicle = Vehicle.objects.filter(VehicleNo=vehicleNoInput).first()
+
+            if vehicle is not None:
+                vehicleNo = vehicle.id
+
+            records = Services.objects.filter(Service_Date__month=month, Service_Date__year=year , VehicleNo=vehicleNo)
+
+            if len(records) > 0:
+
+                total = 0
+
+                for rec in records:
+                    total = total + rec.Amount
+
+                data = {
+                    'Vrepairs': records,
+                    'year':year,
+                    'month':month,
+                    'total': total,
+                    }
+
+                pdf = render_to_pdf('Transport_templates/Maintenance_Report.html', data)
+                return HttpResponse(pdf, content_type='application/pdf')
+
+            else:
+                messages.error(request, 'No Data Found')
+                return redirect('Reports')
+
+        else:
+            messages.error(request, 'Invalid Inputs')
+            return redirect('Reports')
