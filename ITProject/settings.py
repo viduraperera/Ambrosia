4ae8bd5e3def9ad4ef1c72f506a6a1e605abae18
Sandun Lakshitha <54465003+Sandun01@users.noreply.github.com>
2020-10-21 13:04:05 +0530

    Update settings.py
    hide secret key

--- ITProject/settings.py
+++ ITProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b()a=&w(_69k-mn^cpkkam#7-^g3+%a!+xttbf$o2nk6cks)v7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
